b9e268fd08718787d3d3102102728dabb9a44693
Buddhamon <wlacey@smu.edu>
2018-02-27 18:31:28 -0600

    Starting over, lesson learned

--- Project/Modulator.m
+++
@@ -1,9 +0,0 @@
-function [outputArg1,outputArg2] = Modulator(x,y)
-%UNTITLED4 Summary of this function goes here
-%   Detailed explanation goes here
-%   Adding a comment YOLO!!
-outputArg1 = x;
-outputArg2 = y;
-end
-
-
